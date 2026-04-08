aef587393a44f240cd844a1deb2b8fd771731866
NikoMV02 <>
2026-04-08 21:00:49 +0000

    Desafío: Automatización del Control de Inventario

--- bucles/desaf_o_automatizaci_n_del_control_de_inventario/main.py
+++ bucles/desaf_o_automatizaci_n_del_control_de_inventario/main.py
@@ -4,11 +4,16 @@ inventory =  {"Bread": [30, 50, 10, False],"Eggs": [120, 200, 40, False],"Milk":
 discount_threshold = 100
 print("Processing started")
 for item in inventory:
-    current_stock, minimum_stock, restock_amount, on_sale = inventory[item]
+    # current_stock, minimum_stock, restock_amount, on_sale = inventory[item]
     print(f"Processing {item}")
+    current_stock, minimum_stock, restock_amount, on_sale = inventory[item]
     while current_stock < minimum_stock:
         current_stock += restock_amount
+        inventory[item][0] = current_stock
+        if current_stock > discount_threshold and not on_sale:
+            inventory[item][3] = True
 print("Processing completed")
+
         
 
     
